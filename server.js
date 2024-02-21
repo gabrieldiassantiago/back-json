1e4d16c316cc071a4ac4addc8524dff485e56ccc
Gabriel Dias <gabrieldiassantiago@hotmail.com>
2024-02-20 23:34:00 -0300

    fsd

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('server.json');
+const router = jsonServer.router('server.json'); // Use o arquivo server.json como seu banco de dados
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(router);
+// Defina uma rota específica para /tags
+server.get('/tags', (req, res) => {
+  res.jsonp({ tags: router.db.get('tags').value() });
+});
+
 server.listen(3000, () => {
-  console.log('JSON Server is running')
+  console.log('JSON Server is running');
 });
