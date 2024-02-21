8432e90305cff88db8a5b7dd42ea39598c59a48d
Gabriel Dias <gabrieldiassantiago@hotmail.com>
2024-02-21 00:07:37 -0300

    vamo ver

--- api/server.js
+++ api/server.js
@@ -1,20 +1,13 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('server.json'); // Use o arquivo server.json como seu banco de dados
+const router = jsonServer.router('server.json');
 const middlewares = jsonServer.defaults();
+
+const port = 3000; // Porta que deseja utilizar
 
 server.use(middlewares);
 server.use(router);
 
-// Defina uma rota específica para /tags
-server.get('/tags', (req, res) => {
-  // Obtenha os dados dos tags do arquivo server.json
-  const tags = router.db.get('tags').value();
-  res.json(tags);
-});
-
-// Defina a porta para o servidor ou use uma porta fornecida pelo ambiente
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+server.listen(port, () => {
+  console.log(`JSON Server está rodando na porta ${port}`);
 });
